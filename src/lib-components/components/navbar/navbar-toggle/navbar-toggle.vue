2e3492478221547cb2d543741a61115f4252dd92
SimonMarcelLinden <info@snerve.de>
2021-08-05 10:24:45 +0200

    Collpase component & event

---
+++ src/lib-components/components/navbar/navbar-toggle/navbar-toggle.vue
@@ -0,0 +1,7 @@
+<template>
+  <s-button type="button" @onClick="onClick"> 
+    <span class="navbar-toggle-icon" /> 
+  </s-button>
+</template>
+
+<script src="./navbar-toggle.ts" lang="ts"></script>
--- src/lib-components/index.ts
+++ src/lib-components/index.ts
@@ -1,2 +1,16 @@
 /* eslint-disable import/prefer-default-export */
-export { default as SimplifyVueSample } from './simplify-vue-sample.vue';
+export { default as SButton } from './components/button/button.vue';
+
+export { default as SNavbar } from './components/navbar/navbar/navbar.vue';
+export { default as SNavbarBrand } from './components/navbar/navbar-brand/navbar-brand.vue';
+export { default as SNavbarToggle } from './components/navbar/navbar-toggle/navbar-toggle.vue';
+
+export { default as SCollapse } from './components/collapse/collapse.vue';
+
+
+// export { default as SimplifyVueSample } from './simplify-vue-sample.vue';
+
+// Export all components as named exports
+// export * as components from './components'
+
+// import * as components from '@/lib-components/components';
