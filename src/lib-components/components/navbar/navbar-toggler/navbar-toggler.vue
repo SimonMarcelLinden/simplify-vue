bca7f69e0f09c3cbc229a14a778b55236de43f86
SimonMarcelLinden <info@snerve.de>
2021-09-01 22:20:05 +0200

    Fixed aria-expanded property

--- src/lib-components/components/navbar/navbar-toggler/navbar-toggler.vue
+++ src/lib-components/components/navbar/navbar-toggler/navbar-toggler.vue
@@ -1,5 +1,5 @@
 <template>
-  <s-button type="button" @onClick="onClick" class="navbar-toggler" v-bind:aria-expanded="toggled" > 
+  <s-button type="button" @onClick="onClick" class="navbar-toggler" :aria-expanded="toggled ? 'true' : 'false'"> 
     <span class="navbar-toggler-icon">
       <i v-if="icon" :class="!toggled ? 'simplify-' + icon : 'simplify-' + toggleIcon" />
     </span>
