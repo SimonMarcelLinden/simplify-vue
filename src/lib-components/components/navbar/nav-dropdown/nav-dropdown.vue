7756037573eca68ca746bb000d9df9c61052f913
SimonMarcelLinden <info@snerve.de>
2021-09-10 18:25:22 +0200

    wrapper for dropdown

--- src/lib-components/components/navbar/nav-dropdown/nav-dropdown.vue
+++ src/lib-components/components/navbar/nav-dropdown/nav-dropdown.vue
@@ -1,14 +1,16 @@
 <template>
-  <li class="nav-item dropdown"
-    @mouseover="hover = true" @mouseleave="hover = false" >
-    <s-nav-link class="dropdown-toggle">
-      <span>{{ text }}</span>
-    </s-nav-link>
+	<li class="nav-item dropdown"
+    	@mouseover="hover = true" @mouseleave="hover = false" >
+    	<s-nav-link class="dropdown-toggle">
+      		<span>{{ text }}</span>
+		</s-nav-link>
 
-    <ul class="dropdown-menu" :class="{ 'show': hover, 'dropdown-menu-right': right }" >
-      <slot></slot>
-    </ul>
-  </li>
+    	<div  class="dropdown-menu" :class="{ 'show': hover, 'dropdown-menu-right': right }" >
+			<ul>
+				<slot></slot>
+			</ul>
+		</div>
+  	</li>
 </template>
 
 <script src="./nav-dropdown.ts" lang="ts"></script>
