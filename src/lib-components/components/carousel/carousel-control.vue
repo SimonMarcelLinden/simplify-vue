2813950ac1947ea2775e86103b353a37c9651e9d
SimonMarcelLinden <info@snerve.de>
2021-09-19 22:56:56 +0200

    Add hidden attribute to carousel control navigation

--- src/lib-components/components/carousel/carousel-control.vue
+++ src/lib-components/components/carousel/carousel-control.vue
@@ -5,14 +5,14 @@
             <span aria-hidden="true" class="carousel-control-prev-icon">
                 <i class="simplify-arrow-back"></i>
             </span>
-            <span class="sr-only">Previous slide</span>
+            <span class="sr-only" hidden>Previous slide</span>
         </a>
         <a href="#" role="button" @click="next" aria-controls="carousel-1___BV_inner_"
             class="carousel-control-next" >
             <span aria-hidden="true" class="carousel-control-next-icon">
                 <i class="simplify-arrow-forward"></i>
             </span>
-            <span class="sr-only">Next slide</span>
+            <span class="sr-only" hidden>Next slide</span>
         </a>
     </div>
 </template>
