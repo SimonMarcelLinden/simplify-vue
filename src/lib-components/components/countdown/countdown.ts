7b35d60d63b3624259c638d53b9c019449e8573c
SimonMarcelLinden <info@snerve.de>
2021-09-24 21:52:28 +0200

    Countdown component

---
+++ src/lib-components/components/countdown/countdown.ts
@@ -0,0 +1,103 @@
+import Vue from "vue";
+
+const STYLES = [
+	1, 2, 3, 4
+];
+
+export default /*#__PURE__*/ Vue.extend({
+	name: "SCountdown",
+	components: {},
+	props: {
+		endDate: {
+			type 	: [Date, String],
+			default	: function() {
+				return new Date();
+			  },
+		},
+        variant: {
+            type 	: Number,
+            required: false,
+			default : 1,
+            validator(value) {
+              const isValid = STYLES.includes(value);
+              if (!isValid) {
+                console.warn(`allowed variants are ${STYLES}`);
+              }
+              return isValid;
+            },
+        },
+	},
+	data() {
+		return{
+			displayDays		: 0,
+			displayHours 	: 0,
+			displayMinutes	: 0,
+			displaySeconds	: 0,
+		}
+	},
+	mounted() {
+		// @ts-ignore-next-lines
+		this.showRemaining();
+	},
+	computed: {
+		_seconds(): number {
+			return 1000;
+		},
+		_minutes(): number {
+			// @ts-ignore-next-lines
+			return this._seconds * 60;
+		},
+		_hours(): number {
+			// @ts-ignore-next-lines
+			return this._minutes * 60;
+		},
+		_days(): number {
+			// @ts-ignore-next-lines
+			return this._hours * 24;
+		},
+		getStyleClass(): null{
+			return null;
+			// return `countdown-style-${this.variant}`
+		},
+	},
+	methods: {
+		formatNum(num: number): String {
+			return num < 10 ? `0${num}` : `${num}`;
+		},
+		showRemaining() {
+			const timer = setInterval(() => {
+				const now = new Date();
+				const end = new Date("2021/09/28 00:00:00");
+				const distance = end.getTime() - now.getTime();
+
+				if(distance < 0 ){
+					clearInterval(timer); 
+					return;
+				}
+
+				// @ts-ignore-next-lines
+				const days 		= Math.floor(distance / this._days);
+				// @ts-ignore-next-lines
+				const hours 	= Math.floor((distance % this._days) / this._hours);
+				// @ts-ignore-next-lines
+				const minutes 	= Math.floor((distance % this._hours) / this._minutes);
+				// @ts-ignore-next-lines
+				const seconds 	= Math.floor((distance % this._minutes) / this._seconds);
+			
+				// @ts-ignore-next-lines
+				this.displayMinutes = this.formatNum(minutes);
+				// @ts-ignore-next-lines
+				this.displaySeconds = this.formatNum(seconds);
+				// @ts-ignore-next-lines
+				this.displayHours 	= this.formatNum(hours);
+				// @ts-ignore-next-lines
+				this.displayDays 	= this.formatNum(days);
+			}, 1000)
+		}
+	},
+	watch : { },
+	beforeDestroy(){
+		// @ts-ignore-next-line
+		clearInterval(this.timer)
+	}
+});
---
+++ src/lib-components/components/countdown/countdown.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="countdown-time mb-4" :class="getStyleClass"
+    data-time="2021/09/02 12:30:15" >
+    <div class="countdown-box">
+      <div class="countdown-wrapper">
+        <div class="countdown countdown-days">{{ displayDays }}</div>
+        <div class="text">Days</div>
+      </div>
+    </div>
+    <div class="countdown-box">
+      <div class="countdown-wrapper">
+        <div class="countdown countdown-hours">{{ displayHours }}</div>
+        <div class="text">Hours</div>
+      </div>
+    </div>
+    <div class="countdown-box">
+      <div class="countdown-wrapper">
+        <div class="countdown countdown-minutes">{{ displayMinutes }}</div
+        ><div class="text">Minutes</div>
+      </div>
+    </div>
+    <div class="countdown-box">
+      <div class="countdown-wrapper">
+        <div class="countdown countdown-seconds">{{ displaySeconds }}</div
+        ><div class="text">Seconds</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script src="./countdown.ts" lang="ts"></script>
